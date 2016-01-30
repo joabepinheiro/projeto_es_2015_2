4715c7df7cb68f9608ddd31884d57a23f820359e
Joabe Pinheiro <joabepinhero@gmail.com>
2016-01-29 23:27:51 -0300

    Update wsgi.py

--- SPLArch/wsgi.py
+++ SPLArch/wsgi.py
@@ -23,9 +23,6 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SPLArch.settings")
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
-
-
 from whitenoise.django import DjangoWhiteNoise
 
 application = get_wsgi_application()
