14839d2fd43138fc709acf55115c9f3bddfceec3
Joabe Pinheiro <joabepinhero@gmail.com>
2016-01-29 23:27:27 -0300

    Update wsgi.py

--- SPLArch/wsgi.py
+++ SPLArch/wsgi.py
@@ -25,6 +25,12 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SPLArch.settings")
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 
+
+from whitenoise.django import DjangoWhiteNoise
+
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
+
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
 # application = HelloWorldApplication(application)
