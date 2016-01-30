e8706a4ce92ec30e37af19534ca42884445aa99e
Joabe Pinheiro <joabepinhero@gmail.com>
2016-01-29 23:15:12 -0300

    Update urls.py

--- SPLArch/urls.py
+++ SPLArch/urls.py
@@ -1,4 +1,6 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls.defaults import patterns, include, url
+from django.contrib import admin
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -19,3 +21,9 @@ urlpatterns = patterns('',
     url(r'^tour/', 'SPLArch.architecture.views.home')
 
 )
+
+
+if not settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    )
