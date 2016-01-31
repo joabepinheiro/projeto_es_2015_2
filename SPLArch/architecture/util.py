8554f9aafe87cb44e2ee0cb0d9d05cec6dc14d43
Joabe Pinheiro <joabepinhero@gmail.com>
2016-01-31 01:05:04 -0300

    teste temp 12

--- SPLArch/architecture/util.py
+++ SPLArch/architecture/util.py
@@ -12,7 +12,7 @@ import shutil
 from SPLArch.architecture.TemporaryDirectory import *
 
 
-def render_to_latex(template, context, context_instance=None):
+def render_to_latex2(template, context, context_instance=None):
     import os, codecs
     body = render_to_string(template, context, context_instance)
     #TODO: there is still a lot of HTML codes to replace here
@@ -24,7 +24,7 @@ def render_to_latex(template, context, context_instance=None):
     body = body.replace('<br>', '\\')
     body = body.replace('#', '\\#')
 
-    tempf = NamedTemporaryFile(dir=os.getcwd()+'/temp')
+    tempf = NamedTemporaryFile(dir=os.getcwd()+'\\temp')
     tempf.close()
     tempf = codecs.open(tempf.name, 'w', 'utf-8')
     tempf.write(body)
@@ -35,3 +35,27 @@ def render_to_latex(template, context, context_instance=None):
     return open(tempf.name + '.pdf', 'rb').read()
 
 
+def render_to_latex(template, context, context_instance=None):
+
+    template = get_template(template)
+    context = Context(context)
+    rendered_tpl = template.render(context).encode('utf-8')
+    # Python3 only. For python2 check out the docs!
+    with TemporaryDirectory(dir=os.getcwd()+'\\temp') as tempdir:
+
+        # Create subprocess, supress output with PIPE and
+        # run latex twice to generate the TOC properly.
+        # Finally read the generated pdf.
+        for i in range(2):
+            process = Popen(
+                ['pdflatex', '-output-directory', tempdir],
+                stdin=PIPE,
+                stdout=PIPE,
+            )
+            process.communicate(rendered_tpl)
+        with open(os.path.join(tempdir, 'texput.pdf'), 'rb') as f:
+            pdf = f.read()
+    r = HttpResponse(content_type='application/pdf')
+    # r['Content-Disposition'] = 'attachment; filename=texput.pdf'
+    r.write(pdf)
+    return r
