698d07df53183af696c763813e7d3a374421c7d5
Joabe Pinheiro <joabepinhero@gmail.com>
2016-01-31 00:30:38 -0300

    teste temp 10

--- SPLArch/architecture/util.py
+++ SPLArch/architecture/util.py
@@ -7,11 +7,12 @@ from django.http import HttpResponse
 from django.template import Context
 from django.template.loader import get_template
 import os
+import shutil
 
 from SPLArch.architecture.TemporaryDirectory import *
 
 
-def render_to_latex2(template, context, context_instance=None):
+def render_to_latex(template, context, context_instance=None):
     import os, codecs
     body = render_to_string(template, context, context_instance)
     #TODO: there is still a lot of HTML codes to replace here
@@ -23,7 +24,7 @@ def render_to_latex2(template, context, context_instance=None):
     body = body.replace('<br>', '\\')
     body = body.replace('#', '\\#')
 
-    tempf = NamedTemporaryFile()
+    tempf = NamedTemporaryFile(dir=os.getcwd()+'\\temp')
     tempf.close()
     tempf = codecs.open(tempf.name, 'w', 'utf-8')
     tempf.write(body)
@@ -34,27 +35,3 @@ def render_to_latex2(template, context, context_instance=None):
     return open(tempf.name + '.pdf', 'rb').read()
 
 
-
-def render_to_latex(template, context, context_instance=None):
-
-    template = get_template(template)
-    context = Context(context)
-    rendered_tpl = template.render(context).encode('utf-8')
-    # Python3 only. For python2 check out the docs!
-    with TemporaryDirectory() as tempdir:
-        # Create subprocess, supress output with PIPE and
-        # run latex twice to generate the TOC properly.
-        # Finally read the generated pdf.
-        for i in range(2):
-            process = Popen(
-                ['pdflatex', '-output-directory', tempdir],
-                stdin=PIPE,
-                stdout=PIPE,
-            )
-            process.communicate(rendered_tpl)
-        with open(os.path.join(tempdir, 'texput.pdf'), 'rb') as f:
-            pdf = f.read()
-    r = HttpResponse(content_type='application/pdf')
-    # r['Content-Disposition'] = 'attachment; filename=texput.pdf'
-    r.write(pdf)
-    return r
